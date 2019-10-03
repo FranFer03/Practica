0d611882c1fbda1509179f64bcac83076b4c0993
FranFer03 <franfer0301@gmail.com>
2019-10-03 02:07:53 -0300

    Añadiendo el archivo hello

---
+++ mayor_de_tres.py
@@ -0,0 +1,7 @@
+primer_numero = int(input("Ingrese el numero : "))
+segundo_numero = int(input("Ingrese el numero : "))
+tercer_numero = int(input("Ingrese el numero : "))
+
+resultado = max(primer_numero, segundo_numero, tercer_numero)
+
+print(resultado)
