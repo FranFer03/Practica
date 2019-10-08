cba357f03a07c5d13dd80b7e5945bf7d8047f263
FranFer03 <franfer0301@gmail.com>
2019-10-08 15:21:46 -0300

    .

--- practice_class_2.py
+++ practice_class_2.py
@@ -1,18 +1,20 @@
 """leg_1 = float(input("Ingrese el cateto : "))
 leg_2 = float(input("Ingrese el 2do cateto : "))
 hypotenuse = float(input("Ingrese la hipotenusa : "))"""
-
 number_1 = float(input("Ingrese el 1er lado : "))
 number_2 = float(input("Ingrese el 2do lado : "))
 number_3 = float(input("Ingrese el 3er lado : "))
 
 if number_1 > 0 and number_2 > 0 and number_3 > 0:
+
     if number_1 > number_2 and number_1 > number_3:
         hypotenuse = number_1
         calculus = number_3**2 + number_2**2
+
     elif number_2 > number_1 and number_2 > number_3:
         hypotenuse = number_2
         calculus = number_3 ** 2 + number_1 ** 2
+
     else:
         hypotenuse = number_3
         calculus = number_2 ** 2 + number_1 ** 2
