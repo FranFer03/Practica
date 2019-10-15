910c66bb81f8022da15b263a7335a673ccdfa526
FranFer03 <franfer0301@gmail.com>
2019-10-15 15:01:50 -0300

    .

---
+++ practice_class_3.1.py
@@ -0,0 +1,18 @@
+import random
+mayor, menor = 0 ,0
+for i in range(0, 50):
+    edad = random.randrange(1, 75)
+    print("", edad ,end="")
+    if edad > 21:
+        mayor += 1
+    elif edad < 21:
+        menor += 1
+
+
+print("\nMayores a 21 : ", end="")
+for i in range(mayor):
+    print("*", end="")
+
+print("\nMenor a 21 : ", end="")
+for i in range(menor):
+    print("*", end="")
