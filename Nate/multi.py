3d697729a2170c64e643350b18cf017591c59fdb
franf <franfer0301@gmail.com>
2023-04-04 02:24:43 -0300

    .

--- Nate/multi.py
+++ Nate/multi.py
@@ -2,4 +2,4 @@ number = int(input("Ingrese numero de tabla : "))
 
 for multiplo in range(1, 11):
     if (number * multiplo) % 2 == 0:
-        print("{} x {} ={}".format(number, multiplo, multiplo * number))
+        print("{} x {} = {}".format(number, multiplo, multiplo * number))
